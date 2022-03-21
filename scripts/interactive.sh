9c80e05cafa65578c271a7dd3eda0b388df975bd
Joseph Suh <joseph.suh@murple.ai>
2022-03-21 16:35:59 +0900

    Init commmit.

---
+++ scripts/interactive.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PORT=${PORT:-8888}
+
+# if gpu not available
+docker run -it --rm --ipc=host -p $PORT:$PORT -v $PWD:/workspace/template_project/ template_project:latest bash
+
+# if gpu available
+#docker run --gpus device=0 -it --rm --ipc=host -p $PORT:$PORT -v $PWD:/workspace/$PROJECT_NAME/ $PROJECT_NAME:latest bash
