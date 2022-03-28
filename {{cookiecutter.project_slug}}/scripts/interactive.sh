faa382901fc36fa58ca61bebb169862ccbbd7c43
Joseph Suh <joseph.suh@murple.ai>
2022-03-28 16:24:35 +0900

    [DELETE] rm parameter in interactive.sh script.

--- {{cookiecutter.project_slug}}/scripts/interactive.sh
+++ {{cookiecutter.project_slug}}/scripts/interactive.sh
@@ -3,7 +3,7 @@
 PORT=${PORT:-8888}
 
 {%- if cookiecutter.use_nvidia_gpu == 'no' %}
-docker run -it --rm --ipc=host -p $PORT:$PORT -p 5000:5000 -v $PWD:/workspace/{{cookiecutter.project_slug}}/ {{cookiecutter.project_slug}}:latest bash
+docker run -it --ipc=host -p $PORT:$PORT -p 5000:5000 -v $PWD:/workspace/{{cookiecutter.project_slug}}/ {{cookiecutter.project_slug}}:latest bash
 {%- else -%}
-docker run --gpus=all -it --rm -e CUDA_VISIBLE_DEVICES --ipc=host -p $PORT:$PORT -p 5000:5000 -v $PWD:/workspace/{{cookiecutter.project_slug}}/ {{cookiecutter.project_slug}}:latest bash
+docker run --gpus=all -it -e CUDA_VISIBLE_DEVICES --ipc=host -p $PORT:$PORT -p 5000:5000 -v $PWD:/workspace/{{cookiecutter.project_slug}}/ {{cookiecutter.project_slug}}:latest bash
 {%- endif %}
